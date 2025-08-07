c2c62a844525c8f9a73dbbbe52e33a0e4ec12d6c
timoteostifft <timoteostifft@gmail.com>
2025-08-07 17:16:39 -0300

    feat: users mock

---
+++ src/database/users.ts
@@ -0,0 +1,20 @@
+// Entities
+import { User } from "../entities/user";
+
+export const users: User[] = [
+  {
+    id: "user123",
+    name: "John Doe",
+    email: "john.doe@example.com",
+  },
+  {
+    id: "user456",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+  },
+  {
+    id: "user789",
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com",
+  },
+];
